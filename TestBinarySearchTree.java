8c3c59c44844b9f3827ea72bc60e5c088133eebe
WenChi <u10416019@go.utaipei.edu.tw>
2016-05-14 22:22:33 +0800

    Create twenty random numbers
    This file has some data fields,  and I also create twenty random numbers then input them in an array, after input twenty random numbers then print them before the binary sort.

---
+++ TestBinarySearchTree.java
@@ -0,0 +1,25 @@
+//U10416019鄭文琪
+
+import java.security.SecureRandom;
+
+public class TestBinarySearchTree{
+	public static void main(String[] args){
+		//data field
+		int[] number = new int[20];
+		SecureRandom random = new SecureRandom();
+		
+		//create twenty random numbers and input them in the array
+		for(int i = 0; i < 20; i++){
+			number[i] = random.nextInt(100);
+		}
+
+		//print the random numbers
+		System.out.println("The random number is : ");
+		for(int i = 0; i < 20; i++){
+			System.out.print(number[i] + "  ");
+		}
+		System.out.println("");
+		System.out.println("");
+		
+	}
+}
